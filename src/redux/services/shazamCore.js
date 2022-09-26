16c55d316c655a55163bab450451aeaa36e46c25
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-09-26 14:39:20 -0300

    Criação de top artista concluda com designer e responsividade criação dos slides de top artista top musica

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,8 +1,6 @@
 import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 
 
-
-
     export const shazamCoreApi = createApi({
        reducerPath: "shazamCoreApi",
        baseQuery: fetchBaseQuery({
