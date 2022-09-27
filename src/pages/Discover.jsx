d3e562662c2c08ab76d44887f7047b6cf789b755
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-09-27 15:38:07 -0300

    seletor de estilo musical concluido com todas funcionalidade completa --- input de busca concluido -- resta apenas alguns ajustes visuais para o fim do projeto MusicApp

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,20 +1,26 @@
 import { useDispatch, useSelector } from "react-redux"
 import {Error, Loader, SongCard} from "../components"
 import {genres} from "../assets/constants"
+import {useGetSongsByGenreQuery} from "../redux/services/shazamCore"
 
-import {useGetTopChartsQuery} from "../redux/services/shazamCore"
+import { selectGenreListId } from "../redux/features/playerSlice"
+
 
 
 
 const Discover = () =>{
     const dispatch = useDispatch()
-    const { activeSong, isPlaying } = useSelector((state)=> state.player)
-    const { data, isFetching, error} = useGetTopChartsQuery();
-   const genreTitle = "Pop";
+    
+    const { activeSong, isPlaying, genreListId } = useSelector((state)=> state.player)
+    const { data, isFetching, error} = useGetSongsByGenreQuery( genreListId || "POP");
+   
 
 
    if(isFetching) return<Loader title="Loading songs..."/>
    if(error) return <Error/>
+
+
+   const genreTitle = genres.find(({value})=> value === genreListId)?.title
 
 
 
@@ -23,8 +29,8 @@ return(
     <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
     <h2 className="font-bold text-3x1 text-white text-left" >Buscar {genreTitle}</h2>
        <select
-       onChange={()=> {}}
-       value=""
+       onChange={(e)=> dispatch(selectGenreListId(e.target.value))}
+       value={genreListId || "POP"}
        className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none s,:mt0 mt-05"
        > {genres.map((genre)=><option key={genre.value} value={genre.value}>{genre.title}</option>)}</select>
     </div>
